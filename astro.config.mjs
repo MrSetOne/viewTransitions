1c16e666c8bc81456e996573bbaf5236e0b05935
Michael Lara Sánchez <lara.sanchez.michael@gmail.com>
2023-09-07 13:48:45 +0200

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  site:'https://view-transitions-bcna.vercel.app/',
+  integrations: [tailwind(), react(), prefetch(), sitemap()]
 });
